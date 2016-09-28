e040ef81402cb3f973be60300c9349e7dfe90fb9
Younix <foreveryounix@gmail.com>
2016-09-28 15:19:51 +0800

    Learn C the Hard Way: ex23: duff device

--- program/c/p23认识达夫设备/duff.c
+++ program/c/p23认识达夫设备/duff.c
@@ -21,13 +21,13 @@ int duffs_device(char *from, char *to, int count)
 
         switch(count % 8) {
             case 0: do { *to++ = *from++;
-                        case 7: *to++ = *from++;
-                        case 6: *to++ = *from++;
-                        case 5: *to++ = *from++;
-                        case 4: *to++ = *from++;
-                        case 3: *to++ = *from++;
-                        case 2: *to++ = *from++;
-                        case 1: *to++ = *from++;
+            case 7: *to++ = *from++;
+            case 6: *to++ = *from++;
+            case 5: *to++ = *from++;
+            case 4: *to++ = *from++;
+            case 3: *to++ = *from++;
+            case 2: *to++ = *from++;
+            case 1: *to++ = *from++;
                     } while(--n > 0);
         }
     }
