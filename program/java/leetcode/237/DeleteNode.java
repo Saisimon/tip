89d574f24bc4cf692930a4b917a40cf3d1c262c8
Saisimon <13260547845@163.com>
2016-10-06 10:09:52 +0800

    237-Delete Node in a Linked List

---
+++ program/java/leetcode/237/DeleteNode.java
@@ -0,0 +1,34 @@
+package net.saisimon.leetcode;
+
+/**
+ * 237. Delete Node in a Linked List
+ * 	Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * 	Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ * 	
+ * 	Definition for singly-linked list.
+ * 	public class ListNode {
+ *     	int val;
+ *     	ListNode next;
+ *     	ListNode(int x) { val = x; }
+ * 	}
+ * 
+ * @author Saisimon
+ *
+ */
+public class DeleteNode {
+	public void deleteNode(ListNode node) {
+        if (node != null && node.next != null) {
+        	ListNode nNode = node.next;
+        	node.val = nNode.val;
+        	node.next = nNode.next;
+        }
+    }
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+}
