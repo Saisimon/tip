a03d058178d9f28cabd2ee4c82438000c339a5f7
Younix <foreveryounix@gmail.com>
2016-10-18 08:42:53 +0800

    LinuxC Tip: ELF Learnning

---
+++ linuxC/Tools_Usage/ELF_learnning/myprintf.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: myprintf.c
+    > Author: Younix
+    > Mail: foreveryounix@gmail.com 
+    > Created Time: 2016年10月17日 星期一 15时10分43秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+void myprintf(void)
+{
+    printf("hello,world!\n");
+}
