0003c89ba2c859ef62df9650283c9bbf54404f9f
Saisimon <13260547845@163.com>
2016-10-27 21:07:36 +0800

    386-Lexicographical Numbers

---
+++ program/java/leetcode/386/LexicographicalNumbers.java
@@ -0,0 +1,42 @@
+package net.saisimon.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <h1>386. Lexicographical Numbers</h1>
+ * 	<p>Given an integer n, return 1 - n in lexicographical order.
+ * 
+ * 	<p>For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+ * 	
+ * 	<p>Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+ * 
+ * @author Saisimon
+ *
+ */
+public class LexicographicalNumbers {
+	public List<Integer> lexicalOrder(int n) {
+		List<Integer> res = new LinkedList<Integer>();
+		int idx = 1;
+		//当前数
+		int tmp = 1;
+        while (idx <= n) {
+        	if (tmp <= n && tmp * 10 <= n) { // 当前数小于 n，10 倍当前数也小于 n
+        		idx++;
+        		res.add(tmp);
+        		tmp *= 10;
+        	} else if (tmp <= n && tmp * 10 > n) { // 当前数小于 n，10 倍的当前数大于 n
+        		idx++;
+        		res.add(tmp);
+        		while (tmp % 10 == 9) { // 当前数末尾为9的话，下个数为除 10 除到没有 9 为止加上 1
+        			tmp /= 10;
+        		}
+        		tmp++;
+        	} else { // 当前数超过 n
+        		tmp /= 10;
+        		tmp++;
+        	}
+        }
+        return res;
+    }
+}
