677e1750f98db50de6a425c15179a0d1b50c2d21
Saisimon <13260547845@163.com>
2016-12-24 17:23:19 +0800

    Learn Python The Hard Way - 03

---
+++ program/python/Learn_Python_The_Hard_Way/03/ex3.py
@@ -0,0 +1,22 @@
+﻿# print
+print "I will now count something:"
+
+# + /
+print "Hens", 25 + 30 / 6
+# - * %
+print "Roosters", 100 - 25 * 3 % 4
+
+# + - / %
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
+
+# <
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# > >= <=
+print "5 > -2?", 5 > -2
+print "5 >= -2?", 5 >= -2
+print "5 <= -2?", 5 <= -2
