04c8bbcdc27c26e61bc04343edce3b9c91fa7eec
Saisimon <13260547845@163.com>
2016-12-26 11:30:46 +0800

    Learn Python The Hard Way - 08

---
+++ program/python/Learn_Python_The_Hard_Way/08/ex8.py
@@ -0,0 +1,14 @@
+﻿formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one","two","three","four")
+print formatter % (True, False, True, False)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn`t sing.",
+	"So I said goodnight."
+)
+print formatter % ("一","二","三","四")
+print "%s %s %s %s" % ("一","二","三","四")
