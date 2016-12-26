0f11b21b3ac3afafcee2e1af2faacd0990f3dbff
Saisimon <13260547845@163.com>
2016-12-26 12:01:39 +0800

    Learn Python The Hard Way - 10

---
+++ program/python/Learn_Python_The_Hard_Way/10/ex10.py
@@ -0,0 +1,26 @@
+﻿# \n 换行
+# \t Tab
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+# 多行字符串
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+print fat_cat
+
+print "%r" % fat_cat
