10cf06ff9802c863f42e7107a47a46c491c0b8bb
Saisimon <13260547845@163.com>
2017-01-03 12:00:10 +0800

    Learn Python The Hard Way - 46

---
+++ program/python/Learn_Python_The_Hard_Way/46/pip.ini.py
@@ -0,0 +1,14 @@
+﻿import os
+
+ini="""[global]
+index-url = https://pypi.doubanio.com/simple/
+[install]
+trusted-host=pypi.doubanio.com
+"""
+pippath=os.environ["USERPROFILE"]+"\\pip\\"
+
+if not os.path.exists(pippath):
+    os.mkdir(pippath)
+
+with open(pippath+"pip.ini","w+") as f:
+    f.write(ini)
---
+++ program/python/Learn_Python_The_Hard_Way/46/skeleton/setup.py
@@ -0,0 +1,17 @@
+﻿try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+	'description': 'Project',
+	'author': 'Saisimon',
+	'url': 'http://blog.saisimon.net',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'Project'
+}
+
+setup(**config)
