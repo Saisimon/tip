3a7c8597f3f9bd1f221e2ff34b6e8867a895a204
Saisimon <13260547845@163.com>
2017-04-06 09:11:29 +0800

    Java 7 Concurrency Cookbook - 线程组

--- program/java/java7_concurrency_cookbook/net/saisimon/main/Main.java
+++ program/java/java7_concurrency_cookbook/net/saisimon/main/Main.java
@@ -18,7 +18,9 @@ import net.saisimon.main.concurrent.FileClock;
 import net.saisimon.main.concurrent.FileSearch;
 import net.saisimon.main.concurrent.NetworkConnectionsLoader;
 import net.saisimon.main.concurrent.PrimeGenerator;
+import net.saisimon.main.concurrent.Result;
 import net.saisimon.main.concurrent.SafeTask;
+import net.saisimon.main.concurrent.SearchTask;
 import net.saisimon.main.concurrent.ThrowUncaughtExceptionTask;
 import net.saisimon.main.concurrent.UnsafeTask;
 import net.saisimon.main.concurrent.WriterTask;
@@ -39,8 +41,9 @@ public class Main {
 //		dateSourcesLoaderAndNetworkConnectionsLoaderMain();
 //		writerTaskAndCleanerTaskMain();
 //		throwUncaughtExceptionTaskMain();
-		unsafeTaskMain();
-		safeTaskMain();
+//		unsafeTaskMain();
+//		safeTaskMain();
+		searchTaskMain();
 	}
 	
 	private static final int THREAD_SIZE = 10;
@@ -248,6 +251,47 @@ public class Main {
 			thread.start();
 			try {
 				TimeUnit.SECONDS.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * http://ifeve.com/thread-management-11/
+	 * 
+	 * @see SearchTask
+	 */
+	public static void searchTaskMain() {
+		// 线程组
+		ThreadGroup threadGroup = new ThreadGroup("Searcher");
+		Result result = new Result();
+		SearchTask task = new SearchTask(result);
+		for (int i = 0; i < 5; i++) {
+			Thread thread = new Thread(threadGroup, task);
+			thread.start();
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.printf("Number of Threads: %d\n", threadGroup.activeCount());
+		System.out.printf("Information about the Thread Group\n");
+		threadGroup.list();
+		Thread[] threads = new Thread[threadGroup.activeCount()];
+		threadGroup.enumerate(threads);
+		for (int i = 0; i < threadGroup.activeCount(); i++) {
+			System.out.printf("Thread %s: %s\n", threads[i].getName(), threads[i].getState());
+		}
+		waitFinish(threadGroup);
+		threadGroup.interrupt();
+	}
+	
+	private static void waitFinish(ThreadGroup threadGroup) {
+		while (threadGroup.activeCount() > 9) {
+			try {
+				TimeUnit.SECONDS.sleep(1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
---
+++ program/java/java7_concurrency_cookbook/net/saisimon/main/concurrent/Result.java
@@ -0,0 +1,14 @@
+package net.saisimon.main.concurrent;
+
+public class Result {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
